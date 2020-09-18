0674574e3b885f924d32860c19b7517abe5b3594
Kimo Silva <e.k.silva441@gmail.com>
2020-09-18 14:06:34 -0700

    basic styling added

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -16,11 +16,28 @@ class App extends React.Component {
 
   render() {
     return(
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Layer1>
+        <H1>Contact Form</H1>
+      </Layer1>
     )
   }
+
 }
 
+const Layer1 = styled.div`
+padding: 15px;
+padding-left: 45px;
+margin: 50px;
+height: 700px;
+width: 500px;
+border-radius: 40px;
+background: #3b3fba;
+box-shadow:  13px 13px 26px #292c82, 
+             -13px -13px 26px #4d52f2;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Ubuntu', sans-serif;
+  color: #f8c53e;
+`;
 ReactDOM.render(<App />, document.getElementById('App'));
